80c47890aeb6fde73a55e117aeeee8d9598f7c63
Shun Gao <theodore.gao.1986@gmail.com>
2018-01-11 01:09:02 -0800

    change greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ message: "hi" });
+  res.send({ message: "hi there!" });
 });
 
 const PORT = process.env.PORT || 3000;
