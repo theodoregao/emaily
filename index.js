2f01a6796bb3b764b2bdc23f067de9d608553e48
Shun Gao <theodore.gao.1986@gmail.com>
2018-01-12 00:46:14 -0800

    implement google OAuth.

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
+
+mongoose.connect(keys.mongoURI, {
+  useMongoClient: true
+  /* other options */
+});
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ message: "hi there!" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
-console.log("server is ready on", PORT);
+console.log(new Date().toString(), 'server is ready on port', PORT);
---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+
+module.exports = app => {
+  app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+      scope: ['profile', 'email']
+    })
+  );
+
+  app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    console.log('/api/logout');
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    console.log('/api/current_user');
+    res.send(req.user);
+  });
+};
