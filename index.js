f33ea967c8a30ea120e38f8a19fe0537b8132e23
Shun Gao <theodore.gao.1986@gmail.com>
2018-01-12 23:11:52 -0800

    change backend port to 5000.

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 console.log(new Date().toString(), 'server is ready on port', PORT);
