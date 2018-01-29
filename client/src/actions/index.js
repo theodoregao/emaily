eb198e415b589c60f5d32e369ddce5af0b6897c9
Shun Gao <theodore.gao.1986@gmail.com>
2018-01-28 18:29:49 -0800

    fixing handleToken bug

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,6 +8,6 @@ export const fetchUser = () => async dispatch => {
 };
 
 export const handleToken = token => async dispatch => {
-  const res = await axios.get('/api/stripe');
+  const res = await axios.post('/api/stripe', token);
   dispatch({ type: FETCH_USER, payload: res.data });
 };
