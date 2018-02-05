78cfa1f8d9649b0aa934c274b0c651c8c4b5f20a
Shun Gao <theodore.gao.1986@gmail.com>
2018-02-04 16:47:24 -0800

    implement survey form.

---
+++ client/src/utils/validateEmails.js
@@ -0,0 +1,14 @@
+// const regexPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const regexPattern = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+export default emails => {
+  const invalidEmails = emails
+    .split(',')
+    .map(email => email.trim())
+    .filter(email => !regexPattern.test(email));
+
+  if (invalidEmails.length) {
+    return `These emails are invalid: ${invalidEmails}`;
+  }
+  return;
+};
