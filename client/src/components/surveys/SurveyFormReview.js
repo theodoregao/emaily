bbdad2bc0a2f5052fd0a1e85e3c9d9deff2688af
Shun Gao <theodore.gao.1986@gmail.com>
2018-02-20 22:00:13 -0800

    implement client side create survey form.

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,13 +1,15 @@
 import _ from 'lodash';
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import formFields from './formFields';
+import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
   const reviewFields = _.map(formFields, ({ name, label }) => {
     return (
-      <div>
+      <div key={label}>
         <label>{label}</label>
         <div>{formValues[name]}</div>
       </div>
@@ -17,8 +19,12 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
     <div>
       <h5>Please confirm your entries</h5>
       {reviewFields}
-      <button className="yellow darken-3 btn-flat" onClick={onCancel}>
+      <button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
         Back
+      </button>
+      <button className="green white-text btn-flat right" onClick={() => submitSurvey(formValues, history)}>
+        Send Survey
+        <i className="material-icons right">email</i>
       </button>
     </div>
   );
@@ -28,4 +34,4 @@ function mapStateToProps(state) {
   return { formValues: state.form.surveyForm.values };
 }
 
-export default connect(mapStateToProps)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
